2168bdc398f2087643c569238854101ecf3db85e
Yajahira <yari@yvelazquez.com>
2022-06-24 10:33:14 -0400

    modularized weather and movie routes

---
+++ modules/movie.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const axios = require('axios');
+
+async function getMovie (request, response, next){
+
+  try {
+    let movieName = request.query.searchQuery;
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${movieName}`;
+    let movieData = await axios.get(url);
+    console.log(movieData.data);
+    let filmData = movieData.data.results.map(film => new Movie(film));
+    response.send(filmData);
+    console.log(filmData);
+  }
+  catch (error) {
+    handleError(error, response);
+    next(error);
+  }
+
+};
+
+class Movie {
+  constructor(film) {
+    this.title = film.title;
+    this.date = film.release_date;
+  }
+}
+
+module.exports = getMovie;
