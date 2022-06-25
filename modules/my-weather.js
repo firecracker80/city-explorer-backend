b62c2a85fc1400d5f3b84c8a3133ce735108d1a6
Yajahira <yari@yvelazquez.com>
2022-06-24 22:41:17 -0400

    added starter code files

---
+++ modules/my-weather.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const axios = require('axios');
+
+async function getWeather (request, response, next){
+  
+  try{
+    let cityName = request.query.searchQuery;
+    console.log(cityName);
+    let lat = request.query.lat;
+    console.log(lat);
+    let lon = request.query.lon;
+    console.log(lon);
+    let url = `http://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lang=en&units=I&days=5&lat=${lat}&lon=${lon}`;
+    let weatherInfo = await axios.get(url);
+    console.log(weatherInfo);
+    let weatherData = weatherInfo.data.data.map(skies => new Forecast(skies));
+    console.log(weatherData);
+    response.send(weatherData)
+  }
+  catch(error){
+    handleError(error, response);
+    next(error);
+  }
+};
+
+
+class Forecast {
+  constructor(skies) {
+    this.date = skies.datetime;
+    this.description = skies.weather.description;
+  }
+}
+
+const handleError = (error, response) => {
+  response.status(500).send('Something went wrong.')
+}
+
+module.exports = getWeather;
---
+++ my-server.js
@@ -0,0 +1,33 @@
+'use strict';
+//REQUIRE
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+
+//REQUIRED MODULES
+const getWeather = require('./modules/my-weather');
+const getMovie = require('./modules/movie');
+
+//USE
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 3002;
+//ROUTES
+app.get('/', (request, response) => {
+  response.send('hello from our server');
+})
+
+app.get('/hello', (request, response) => {
+
+  response.send('hello')
+})
+
+app.get('/weather', getWeather);
+
+app.get('/movie', getMovie); 
+
+app.get('*', (request, response) => {
+  response.send('What you are looking for, doesn\'t exist');
+})
+//LISTEN
+app.listen(PORT, () => console.log(`LISTENING ON PORT ${PORT}`));
